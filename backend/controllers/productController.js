5ff3a3cc57cb6ccdf1a21cb547f66e9f5ef7b52e
Prajyot-11 <prajyotwalunj11@gmail.com>
2025-05-13 22:46:55 +0530

    Update product controller and route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -114,3 +114,45 @@ exports.getProductDetails = catchAsyncError(async(req,res,next) =>{
         product,
     })
 });
+
+// create a new review or update it
+exports.createProductReview = catchAsyncError(async(req, res, next) => {
+
+      const {rating,comment,productId} = req.body;  
+      
+      const review = {
+          user: req.user._id,
+          name: req.user.name,
+          rating: Number(rating),
+          comment,
+      };
+
+      const product = await Product.findById(productId);
+      const isReviewed = product.reviews.find((rev)=>rev.user.toString() === req.user._id.toString());
+
+      if(isReviewed) {
+        product.reviews.forEach((rev) => {
+            if(rev.user.toString() === req.user._id.toString()) 
+                (rev.rating = rating, rev.comment = comment);
+        });
+      }
+      else {
+        product.reviews.push(review);
+        product.numOfReviews = product.reviews.length
+      }
+
+      let sumOfRatings = 0;
+      product.reviews.forEach(rev => {
+        sumOfRatings += rev.rating;
+      })
+
+      product.ratings = sumOfRatings / product.reviews.length;
+
+      await product.save({
+          validateBeforeSave: false
+      })
+
+      res.status(200).json({
+          success: true,
+      })
+});
