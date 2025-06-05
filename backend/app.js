618f644eb3d125e5554d81ccc3514778f99e9e14
Prajyot-11 <prajyotwalunj11@gmail.com>
2025-06-06 01:29:48 +0530

    `Added orderRoute, orderController, orderModel files and updated app.js to include it`

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,12 @@ app.use(cookieParser());
 // Route Import
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 // Define your routes
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //Middleware for error
 app.use(errorMiddleware); 
